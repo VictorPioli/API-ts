4d94ca34022415e55d21f8b1221841e6505ae73f
Victor Oliveira <victor.oliveira@frenet.com.br>
2026-03-16 17:44:41 -0300

    API - Clean Architecture

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export async function connectMongo() {
-  await mongoose.connect("mongodb+srv://DevUser1:m2Mdmtcb0hH7Rpre@dev.otbneg5.mongodb.net/?appName=Dev");
+  await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@dev.otbneg5.mongodb.net/?appName=Dev`);
 
   console.log("MongoDB conectado");
 }
